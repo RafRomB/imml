e0670f42e43a79fb8505dee9681a5c4663fc3197
alberl <a.l.sanchez@medisin.uio.no>
2023-08-14 11:47:03 +0200

    Attempt DeepMF

--- imvc/transformers/__init__.py
+++ imvc/transformers/__init__.py
@@ -3,4 +3,5 @@ from .compose import DropView, ConcatenateViews, SingleView, AddMissingViews, So
 from .multi_view_transformer import MultiViewTransformer
 from .convert_to_nm import ConvertToNM, convert_to_nm
 from .fill_missing_views import FillMissingViews
+from .deepmf import DeepMF
 
