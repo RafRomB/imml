05ce28bc5579edd4a184c658277deb2a25f82103
alberl <a.l.sanchez@medisin.uio.no>
2023-12-12 13:31:17 +0100

    Parallel work, simulated data and metabric

--- complete_algorithms_evaluation.py
+++ complete_algorithms_evaluation.py
@@ -1,6 +1,7 @@
 import os.path
 import argparse
 import numpy as np
+from pandarallel import pandarallel
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, FunctionTransformer
@@ -24,9 +25,26 @@ random_state = 42
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-continue_benchmarking', default= False, action='store_true')
+parser.add_argument('-n_jobs', default= 1, type= int)
 args = parser.parse_args()
 
-datasets = ["nutrimouse_genotype", "nutrimouse_diet", "bbcsport", "bdgp", "caltech101", "digits", "tcga_tissue", "tcga_survival", "nuswide", "metabric"]
+if args.n_jobs > 1:
+    pandarallel.initialize(nb_workers= args.n_jobs)
+
+datasets = [
+    "simulated_InterSIM",
+    "simulated_netMUG",
+    "nutrimouse_genotype",
+    "nutrimouse_diet",
+    "bbcsport",
+    "buaa",
+    "metabric",
+    "digits",
+    "bdgp",
+    "tcga",
+    "caltech101",
+    "nuswide",
+]
 probs = np.arange(100, step= 10)
 imputation = [True, False]
 runs_per_alg = np.arange(10)
@@ -61,39 +79,42 @@ if not args.continue_benchmarking:
         results = results.merge(pd.Series(v, name= k), how= "cross")
     results = results.set_index(list(indexes_results.keys()))
     results[["finished", "completed"]] = False
+
+    os.remove(logs_file) if os.path.exists(logs_file) else None
+    os.remove(error_file) if os.path.exists(error_file) else None
+    open(logs_file, 'w').close()
+    open(error_file, 'w').close()
 else:
     results = pd.read_csv(file_path, index_col= list(indexes_results.keys()))
     results_ = results.select_dtypes(object).drop(columns= ["comments", "stratified"]).replace(np.nan, "np.nan")
     for col in results_.columns:
         results[col] = results_[col].apply(eval)
-        
-    os.remove(logs_file) if os.path.exists(logs_file) else None
-    os.remove(error_file) if os.path.exists(error_file) else None
-    open(logs_file, 'w').close()
-    open(error_file, 'w').close()
 
 unfinished_results = results.loc[~results["finished"]]
 
 for dataset_name in unfinished_results.index.get_level_values("dataset").unique():
     names = dataset_name.split("_")
+    if "simulated" in names:
+        names = ["_".join(names)]
     x_name,y_name = names if len(names) > 1 else (names[0], "0")
     Xs, y = LoadDataset.load_dataset(dataset_name=x_name, return_y=True, shuffle= False)
     y = y[y_name]
     n_clusters = y.nunique()
 
-    iterator = pd.Series(unfinished_results.loc[[dataset_name]].index.to_list())
-    iterator.apply(lambda x: Utils.run_iteration(idx= x, results= results, Xs=Xs, y=y, n_clusters=n_clusters,
-                                                 algorithms=algorithms, random_state=random_state, file_path=file_path,
-                                                 logs_file=logs_file, error_file=error_file))
-
-    # unfinished_results_dataset = unfinished_results.loc[[dataset_name]]
-    # unfinished_results_dataset_idx = unfinished_results_dataset.xs(0, level=2, drop_level=False).index
-    # iterator = pd.Series(unfinished_results_dataset_idx.to_list())
-    # iterator.parallel_apply(lambda x: Utils.run_iteration(idx= x, results= results, Xs=Xs, y=y, n_clusters=n_clusters,
-    #                                              algorithms=algorithms, random_state=random_state, file_path=file_path,
-    #                                              logs_file=logs_file, error_file=error_file))
-    # unfinished_results_dataset_idx = unfinished_results_dataset.drop(unfinished_results_dataset_idx).index
-    # iterator = pd.Series(unfinished_results_dataset_idx.to_list())
-    # iterator.parallel_apply(lambda x: Utils.run_iteration(idx= x, results= results, Xs=Xs, y=y, n_clusters=n_clusters,
-    #                                              algorithms=algorithms, random_state=random_state, file_path=file_path,
-    #                                              logs_file=logs_file, error_file=error_file))
+    if args.n_jobs == 1:
+        iterator = pd.Series(unfinished_results.loc[[dataset_name]].index.to_list())
+        iterator.apply(lambda x: Utils.run_iteration(idx= x, results= results, Xs=Xs, y=y, n_clusters=n_clusters,
+                                                     algorithms=algorithms, random_state=random_state, file_path=file_path,
+                                                     logs_file=logs_file, error_file=error_file))
+    else:
+        unfinished_results_dataset = unfinished_results.loc[[dataset_name]]
+        unfinished_results_dataset_idx = unfinished_results_dataset.xs(0, level=2, drop_level=False).index
+        iterator = pd.Series(unfinished_results_dataset_idx.to_list())
+        iterator.parallel_apply(lambda x: Utils.run_iteration(idx= x, results= results, Xs=Xs, y=y, n_clusters=n_clusters,
+                                                     algorithms=algorithms, random_state=random_state, file_path=file_path,
+                                                     logs_file=logs_file, error_file=error_file))
+        unfinished_results_dataset_idx = unfinished_results_dataset.drop(unfinished_results_dataset_idx).index
+        iterator = pd.Series(unfinished_results_dataset_idx.to_list())
+        iterator.parallel_apply(lambda x: Utils.run_iteration(idx= x, results= results, Xs=Xs, y=y, n_clusters=n_clusters,
+                                                     algorithms=algorithms, random_state=random_state, file_path=file_path,
+                                                     logs_file=logs_file, error_file=error_file))
