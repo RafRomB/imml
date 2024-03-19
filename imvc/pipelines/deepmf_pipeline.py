Renamed fill_incomplete_samples.py. Adding incomplete_algorithms_evaluation.py Fixing imputer to have always at least one view. Normalizing DAIMC

imvc/pipelines/deepmf_pipeline.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 
-from ..transformers import FillMissingViews, SortData, ConcatenateViews
+from ..transformers import FillIncompleteSamples, SortData, ConcatenateViews
 from . import BasePipeline
 
 
@@ -50,7 +50,7 @@ class DeepMFPipeline(BasePipeline):
 
     def __init__(self, n_clusters: int = None, memory=None, verbose=False, random_state : int = None, **args):
         estimator = KMeans(n_clusters = n_clusters, random_state=random_state)
-        transformers = [SortData(), FillMissingViews(value="mean"), ConcatenateViews(),
+        transformers = [SortData(), FillIncompleteSamples(value="mean"), ConcatenateViews(),
                         StandardScaler().set_output(transform='pandas'), FunctionTransformer(np.concatenate),
                         StandardScaler().set_output(transform='pandas')]
         super().__init__(estimator = estimator, transformers = transformers, memory = memory, verbose = verbose,

imvc/pipelines/mrgcn_pipeline.py
@@ -3,7 +3,7 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 
 from . import BasePipeline
-from ..transformers import SortData, FillMissingViews, MultiViewTransformer
+from ..transformers import SortData, FillIncompleteSamples, MultiViewTransformer
 
 
 class DeepMFPipeline(BasePipeline):
@@ -40,7 +40,7 @@ class DeepMFPipeline(BasePipeline):
 
     def __init__(self, n_clusters: int = None, memory=None, verbose=False, random_state : int = None, **args):
         estimator = KMeans(n_clusters = n_clusters, random_state=random_state)
-        transformers = [SortData(), FillMissingViews(value="mean"),
+        transformers = [SortData(), FillIncompleteSamples(value="mean"),
                         StandardScaler().set_output(transform='pandas'),
                         MultiViewTransformer(FunctionTransformer(lambda x: torch.from_numpy(x.values).float()))]
         super().__init__(estimator = estimator, transformers = transformers, memory = memory, verbose = verbose,