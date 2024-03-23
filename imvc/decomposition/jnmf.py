d4a94489f9e8c8f7ff550e3ca4e420cc545f8235
albertolzs <albertolz@proton.me>
2024-03-23 14:26:40 +0100

    jNMF finished

--- imvc/decomposition/jnmf.py
+++ imvc/decomposition/jnmf.py
@@ -1,15 +1,18 @@
+import pandas as pd
 import numpy as np
 from rpy2.robjects.packages import importr
+import rpy2.robjects as ro
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from ..utils import Utils, check_Xs
 
 
-class JNMF(object):
+class jNMF(TransformerMixin, BaseEstimator):
     r"""
     jNMF (Joint Non-negative Matrix Factorization Algorithms).
 
     jNMF decompose the matrices to two low-dimensional factor matrices. It can deal with both view- and
-    single-wise missing.
+    single-wise missing. It cannot transform new data.
 
     R library "nnTensor" should be installed. This can be done using the R command: install.packages("nnTensor")
 
@@ -55,45 +58,39 @@ class JNMF(object):
 
     References
     ----------
-    [paper1] Hong-Qiang Wang, Chun-Hou Zheng, Xing-Ming Zhao, jNMFMA: a joint non-negative matrix factorization
-            meta-analysis of transcriptomics data, Bioinformatics, Volume 31, Issue 4, February 2015, Pages 572–580,
-            https://doi.org/10.1093/bioinformatics/btu679.
-    [paper2] Liviu Badea, (2008) Extracting Gene Expression Profiles Common to Colon and Pancreatic Adenocarcinoma
+    [paper1] Liviu Badea, (2008) Extracting Gene Expression Profiles Common to Colon and Pancreatic Adenocarcinoma
             using Simultaneous nonnegative matrix factorization. Pacific Symposium on Biocomputing 13:279-290.
-.
-    [paper3] Shihua Zhang, et al. (2012) Discovery of multi-dimensional modules by integrative analysis of cancer
+    [paper2] Shihua Zhang, et al. (2012) Discovery of multi-dimensional modules by integrative analysis of cancer
             genomic data. Nucleic Acids Research 40(19), 9379-9391.
-.
-    [paper4] Zi Yang, et al. (2016) A non-negative matrix factorization method for detecting modules in heterogeneous
+    [paper3] Zi Yang, et al. (2016) A non-negative matrix factorization method for detecting modules in heterogeneous
             omics multi-modal data, Bioinformatics 32(1), 1-8.
-.
-    [paper5] Y. Kenan Yilmaz et al., (2010) Probabilistic Latent Tensor Factorization, International Conference on
+    [paper4] Y. Kenan Yilmaz et al., (2010) Probabilistic Latent Tensor Factorization, International Conference on
             Latent Variable Analysis and Signal Separation 346-353.
-.
-    [paper6] N. Fujita et al., (2018) Biomarker discovery by integrated joint non-negative matrix factorization and
+    [paper5] N. Fujita et al., (2018) Biomarker discovery by integrated joint non-negative matrix factorization and
             pathway signature analyses, Scientific Report.
     [code] https://rdrr.io/cran/nnTensor/man/jNMF.html
 
     Examples
     --------
     >>> from imvc.datasets import LoadDataset
-    >>> from imvc.decomposition import DFMF
+    >>> from imvc.decomposition import jNMF
     >>> from imvc.transformers import MultiViewTransformer
     >>> from sklearn.pipeline import make_pipeline
-    >>> from sklearn.preprocessing import StandardScaler
+    >>> from sklearn.preprocessing import MinMaxScaler
     >>> from sklearn.cluster import KMeans
     >>> Xs = LoadDataset.load_dataset(dataset_name="nutrimouse")
-    >>> dfmf = DFMF(n_components = 5).set_output(transform="pandas")
+    >>> transformer = jNMF(n_components = 5).set_output(transform="pandas")
     >>> estimator = KMeans(n_clusters = 3)
-    >>> pipeline = make_pipeline(MultiViewTransformer(StandardScaler().set_output(transform="pandas")), dfmf, estimator)
+    >>> pipeline = make_pipeline(MultiViewTransformer(MinMaxScaler().set_output(transform="pandas")), transformer, estimator)
     >>> labels = pipeline.fit_predict(Xs)
     """
 
 
-    def __init__(self, n_components : int = 10, init_w = np.nan, init_v = np.nan, init_h = np.nan, fix_w: bool = False,
+    def __init__(self, n_components : int = 10, init_w = None, init_v = None, init_h = None, fix_w: bool = False,
                  fix_v: bool = False, fix_h: bool =False, l1_w: float = 1e-10, l1_v: float = 1e-10, l1_h: float = 1e-10,
-                 l2_w: float = 1e-10, l2_v: float = 1e-10, l2_h: float = 1e-10, w = np.nan, algorithm: str = np.nan,
-                 p: float = 1., thr: float = 1e-10, num_iter: int = 100, verbose=0, random_state: int = None):
+                 l2_w: float = 1e-10, l2_v: float = 1e-10, l2_h: float = 1e-10, w = None,
+                 algorithm = ["Frobenius", "KL", "IS", "PLTF"], p: float = 1., thr: float = 1e-10, num_iter: int = 100,
+                 verbose=0, random_state: int = None):
         self.n_components = n_components
         self.init_w = init_w
         self.init_v = init_v
@@ -112,7 +109,7 @@ class JNMF(object):
         self.p = p
         self.thr = thr
         self.num_iter = num_iter
-        self.verbose = verbose
+        self.verbose = bool(verbose)
         self.random_state = random_state
         self.transform_ = None
 
@@ -135,17 +132,38 @@ class JNMF(object):
         self :  returns and instance of self.
         """
         Xs = check_Xs(Xs, force_all_finite='allow-nan')
+        samples = Xs[0].index
         mask = [X.notnull().astype(int) for X in Xs]
-        Xs, mask = Utils.convert_df_to_r_object(Xs), Utils.convert_df_to_r_object(mask)
+        transformed_Xs, transformed_mask = Utils._convert_df_to_r_object(Xs), Utils._convert_df_to_r_object(mask)
+        if self.algorithm is not None:
+            algorithm = ro.vectors.StrVector(self.algorithm)
         base, nnTensor = importr("base"), importr("nnTensor")
-        base.set_seed(self.random_state)
+        if self.random_state is not None:
+            base.set_seed(self.random_state)
+        init_w = ro.NULL if self.init_w is None else self.init_w
+        init_v = ro.NULL if self.init_v is None else self.init_v
+        init_h = ro.NULL if self.init_h is None else self.init_h
+        w = ro.NULL if self.w is None else self.w
+
         w, v, h, recerror, train_recerror, test_recerror, relchange = nnTensor.jNMF(
-            X= Xs, M=mask, initW=self.init_w, initV=self.init_v, initH=self.init_h, fixW=self.fix_w, fixV=self.fix_v,
-            fixH=self.fix_h, L1_W=self.l1_w, L1_V=self.l1_v, L1_H=self.l1_h, L2_W=self.l2_w, L2_V= self.l2_v,
-            L2_H=self.l2_h, J=self.n_components, w=self.w, algorithm=self.algorithm, p=self.p, thr = self.thr,
-            num_iter=self.num_iter, verbose=self.verbose)
-        self.v_, self.relchange_ = v, relchange
-        self.reconstruction_err_, self.train_reconstruction_err_, self.test_reconstruction_err_ = recerror, train_recerror, test_recerror
+            X= transformed_Xs, M=transformed_mask, J=self.n_components,
+            initW=init_w, initV=init_v, initH=init_h,
+            fixW=self.fix_w, fixV=self.fix_v, fixH=self.fix_h,
+            L1_W=self.l1_w, L1_V=self.l1_v, L1_H=self.l1_h,
+            L2_W=self.l2_w, L2_V= self.l2_v, L2_H=self.l2_h,
+            w=w, algorithm=algorithm, p=self.p, thr = self.thr, num_iter=self.num_iter, verbose=self.verbose)
+
+        v = [np.array(mat) for mat in v]
+        h = [np.array(mat) for mat in h]
+        if self.transform_ == "pandas":
+            w = pd.DataFrame(np.array(w), index= samples)
+            v, h = [pd.DataFrame(mat, index= samples) for mat in v], [pd.DataFrame(mat) for mat in h]
+
+        self.w_, self.v_, self.h_ = w, v, h
+        self.reconstruction_err_ = list(recerror)
+        self.train_reconstruction_err_ = list(train_recerror)
+        self.test_reconstruction_err_ = list(test_recerror)
+        self.relchange_ = list(relchange)
         return self
 
 
@@ -167,11 +185,7 @@ class JNMF(object):
         """
         #todo
         Xs = check_Xs(Xs, force_all_finite='allow-nan')
-        relations = [fusion.Relation(X.values, self.t_, t) for X,t in zip(Xs, self.ts_)]
-        fusion_graph = fusion.FusionGraph(relations)
-        transformed_X = self.transformer_.transform(self.t_, fusion_graph, self.fuser_).factor(self.t_)
-        if self.transform_ == "pandas":
-            transformed_X = pd.DataFrame(transformed_X, index= Xs[0].index)
+        transformed_X = self.w_
         return transformed_X
 
 
