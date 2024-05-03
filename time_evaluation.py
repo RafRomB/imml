735cb595888797d6fb58496b48c3a43188ff44ac
albertolzs <albertolz@proton.me>
2024-05-03 16:46:11 +0200

    Adding exception to time_evaluation.py Creating time_evaluation.ipynb

--- time_evaluation.py
+++ time_evaluation.py
@@ -1,7 +1,9 @@
-import itertools
 import os
 import time
+from collections import defaultdict
+from datetime import datetime
 
+import numpy as np
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, FunctionTransformer
@@ -17,6 +19,8 @@ from models import Model
 folder_results = "results"
 filelame = "time_evaluation.csv"
 file_path = os.path.join(folder_results, filelame)
+logs_file = os.path.join(folder_results, 'time_logs.txt')
+error_file = os.path.join(folder_results, 'time_errors.txt')
 
 random_state = 42
 
@@ -63,10 +67,14 @@ if os.path.exists(file_path):
     results = pd.read_csv(file_path, index_col=0)
 else:
     results = pd.DataFrame(0, index=datasets, columns=algorithms.keys())
+    os.remove(logs_file) if os.path.exists(logs_file) else None
+    os.remove(error_file) if os.path.exists(error_file) else None
+    open(logs_file, 'w').close()
+    open(error_file, 'w').close()
 
-for dataset_name, (alg_name, alg) in itertools.product(datasets, algorithms.items()):
-    if results.loc[dataset_name, alg_name] > 0:
-        continue
+errors_dict = defaultdict(int)
+
+for dataset_name in datasets:
     names = dataset_name.split("_")
     if "simulated" in names:
         names = ["_".join(names)]
@@ -75,12 +83,26 @@ for dataset_name, (alg_name, alg) in itertools.product(datasets, algorithms.item
     y = y[y_name]
     n_clusters = y.nunique()
 
-    start_time = time.perf_counter()
-    clusters, _ = Model(alg_name=alg_name, alg=alg).method(train_Xs=Xs, n_clusters=n_clusters,
-                                                               random_state=random_state, run_n=0)
-    elapsed_time = time.perf_counter() - start_time
+    for alg_name, alg in algorithms.items():
+        time_execution = results.loc[dataset_name, alg_name]
+        if (time_execution > 0) or np.isnan(time_execution):
+            continue
+        with open(logs_file, "a") as f:
+            f.write(f'\n {dataset_name} \t {alg_name} \t {datetime.now()}')
 
-    results.loc[dataset_name, alg_name] = elapsed_time
-    results.to_csv(os.path.join(folder_results, filelame))
+        try:
+            start_time = time.perf_counter()
+            clusters, _ = Model(alg_name=alg_name, alg=alg).method(train_Xs=Xs, n_clusters=n_clusters,
+                                                                       random_state=random_state, run_n=0)
+            elapsed_time = time.perf_counter() - start_time
+        except Exception as exception:
+            errors_dict[f"{type(exception).__name__}: {exception}"] += 1
+            with open(error_file, "a") as f:
+                f.write(
+                    f'\n {dataset_name} \t {alg_name} \t {errors_dict} \t {datetime.now()}')
+            elapsed_time = np.nan
+
+        results.loc[dataset_name, alg_name] = elapsed_time
+        results.to_csv(os.path.join(folder_results, filelame))
 
 
