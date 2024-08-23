eb24a67c2942fa155bfa7eeb3f7b27d4cabefa4f
albertolzs <albertolz@proton.me>
2024-08-23 13:58:45 +0200

    Adding parea

--- src/models/models.py
+++ src/models/models.py
@@ -6,6 +6,7 @@ from sklearn.cluster import SpectralClustering
 from sklearn.manifold import spectral_embedding
 from snf import compute
 from torch.utils.data import DataLoader
+from pyrea import clusterer, view, fuser, execute_ensemble, consensus
 from imvc.decomposition import DeepMFDataset
 
 from src.utils import Utils
@@ -14,7 +15,7 @@ from src.utils import Utils
 class Model:
     def __init__(self, alg_name, alg):
         self.alg_name = alg_name
-        self.method = alg_name.lower() if alg_name in ["SNF", "IntNMF", "COCA", "DeepMF"] else "sklearn_method"
+        self.method = alg_name.lower() if alg_name in ["SNF", "IntNMF", "COCA", "DeepMF", "parea"] else "sklearn_method"
         self.method = eval(f"self.{self.method.lower()}")
         self.framework = alg_name.lower() if alg_name in ["GPCA", "AJIVE", "NMF", "DFMF", "MOFA", "jNMF"] else "standard"
         self.framework = eval(f"self.{self.framework.lower()}")
@@ -44,6 +45,45 @@ class Model:
                                             random_state=sc.random_state, eigen_tol=sc.eigen_tol, drop_first=False)
         transformed_Xs = pd.DataFrame(transformed_Xs, index=train_Xs[0].index)
         return clusters, transformed_Xs
+
+
+    def parea(self, train_Xs, n_clusters, random_state, run_n):
+        model = self.alg["alg"]
+        train_Xs = model.fit_transform(train_Xs)
+
+        c_1_type, c_2_type, c_1_pre_type, c_2_pre_type = ('hierarchical',) *4
+        c_1_method, c_1_pre_method = ('ward',) *2
+        c_2_method, c_2_pre_method = ('complete',) *2
+        c_1_k, c_2_k, c_1_pre_k, c_2_pre_k, k_final = (n_clusters,) *5
+        fusion_method = 'disagreement'
+
+        c1 = clusterer(c_1_type, method=c_1_method, n_clusters=c_1_k)
+        c2 = clusterer(c_2_type, method=c_2_method, n_clusters=c_2_k)
+        c1_pre = clusterer(c_1_pre_type, method=c_1_pre_method, n_clusters=c_1_pre_k, precomputed=True)
+        c2_pre = clusterer(c_2_pre_type, method=c_2_pre_method, n_clusters=c_2_pre_k, precomputed=True)
+        views1 = []
+        for v in train_Xs:
+            views1.append(view(v, c1))
+        f = fuser(fusion_method)
+        v_ensemble_1 = view(execute_ensemble(views1, f), c1_pre)
+        views2 = []
+        for v in train_Xs:
+            views2.append(view(v, c2))
+        v_ensemble_2 = view(execute_ensemble(views2, f), c2_pre)
+        v_res = execute_ensemble([v_ensemble_1, v_ensemble_2], f)
+        if k_final:
+            c_final = clusterer(c_1_pre_type, method=c_1_pre_method, n_clusters=k_final, precomputed=True)
+            v_res_final = view(v_res, c_final)
+            labels = v_res_final.execute()
+
+            labels = labels[:, 0]
+        else:
+            v1_res = view(v_res, c1_pre)
+            v2_res = view(v_res, c2_pre)
+
+            labels = consensus([v1_res.execute(), v2_res.execute()])
+        transformed_Xs = pd.DataFrame(v_res, index=train_Xs[0].index)
+        return labels, transformed_Xs
 
 
     def intnmf(self, train_Xs, n_clusters, random_state, run_n):
