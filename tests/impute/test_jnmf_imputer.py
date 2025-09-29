601f011e4f3fd15bc44907b7cd640a36649af56e
albertolzs <albertolz@proton.me>
2025-09-30 01:51:42 +0200

    Testing matlab module in windows in ci_test.yml.

--- tests/impute/test_jnmf_imputer.py
+++ tests/impute/test_jnmf_imputer.py
@@ -10,6 +10,15 @@ except ImportError:
     rmodule_installed = False
     rmodule_error = "Module 'r' needs to be installed to use r engine."
 
+nnTensor_installed = False
+if rmodule_installed:
+    rbase = importr("base")
+    try:
+        nnTensor = importr("nnTensor")
+        nnTensor_installed = True
+    except PackageNotInstalledError:
+        pass
+
 
 @pytest.fixture
 def sample_data():
@@ -21,23 +30,28 @@ def sample_data():
     Xs_pandas, Xs_numpy = [X1, X2], [X1.values, X2.values]
     return Xs_pandas, Xs_numpy
 
-def test_default_params(sample_data):
-    if rmodule_installed:
-        transformer = JNMFImputer(random_state=42)
-        for Xs in sample_data:
-            transformed_Xs = transformer.fit_transform(Xs)
-            assert len(transformed_Xs) == len(Xs)
-            assert transformed_Xs[0].shape == Xs[0].shape
 
+@pytest.mark.skipif(not rmodule_installed, reason="Module 'r' needs to be installed to use r engine.")
+@pytest.mark.skipif(not nnTensor_installed, reason="nnTensor is not installed.")
+def test_default_params(sample_data):
+    transformer = JNMFImputer(random_state=42)
+    for Xs in sample_data:
+        transformed_Xs = transformer.fit_transform(Xs)
+        assert len(transformed_Xs) == len(Xs)
+        assert transformed_Xs[0].shape == Xs[0].shape
+
+
+@pytest.mark.skipif(not rmodule_installed, reason="Module 'r' needs to be installed to use r engine.")
+@pytest.mark.skipif(not nnTensor_installed, reason="nnTensor is not installed.")
 def test_transform(sample_data):
-    if rmodule_installed:
-        transformer = JNMFImputer(random_state=42)
-        for Xs in sample_data:
-            transformer.fit(Xs)
-            transformed_Xs = transformer.transform(Xs)
-            assert len(transformed_Xs) == len(Xs)
-            assert transformed_Xs[0].shape == Xs[0].shape
-            assert not any([np.isnan(transformed_X).any().any() for transformed_X in transformed_Xs])
+    transformer = JNMFImputer(random_state=42)
+    for Xs in sample_data:
+        transformer.fit(Xs)
+        transformed_Xs = transformer.transform(Xs)
+        assert len(transformed_Xs) == len(Xs)
+        assert transformed_Xs[0].shape == Xs[0].shape
+        assert not any([np.isnan(transformed_X).any().any() for transformed_X in transformed_Xs])
+
 
 if __name__ == "__main__":
     pytest.main()
