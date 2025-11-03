ded0cde3bcee3ac54bca8c973bb876784f47733d
albertolzs <albertolz@proton.me>
2025-11-03 17:19:03 +0100

    - Updating changelog.rst

--- imml/classify/m3care.py
+++ imml/classify/m3care.py
@@ -84,7 +84,7 @@ class M3Care(LightningModule):
     >>> Xs.append(pd.DataFrame(["docs/figures/graph.png", "docs/figures/logo_imml.png"]))
     >>> Xs.append(pd.DataFrame(["This is the graphical abstract of iMML.", "This is the logo of iMML."]))
     >>> Xs = Amputer(p=0.2, random_state=42).fit_transform(Xs) # this step is optional
-    >>> y = pd.Series(np.random.default_rng(42).integers(0, 2, len(Xs[0])), dtype="Float32")
+    >>> y = pd.Series(np.random.default_rng(42).integers(0, 2, len(Xs[0])), dtype=np.float32)
     >>> train_data = M3CareDataset(Xs=Xs, y=y)
     >>> train_dataloader = DataLoader(dataset=train_data, batch_size=10, shuffle=True)
     >>> trainer = Trainer(max_epochs=1, logger=False, enable_checkpointing=False)
